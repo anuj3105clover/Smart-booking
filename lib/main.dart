0e8965f380b9cf79daa5c3081b4aa67b29299706
anuj3105clover <tiwarianujkumar2000@gmail.com>
2026-02-12 02:46:25 +0530

    Create Smart Booking app with home screen
    Initial implementation of Smart Booking app with a home screen displaying categories.

---
+++ lib/main.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(SmartBookingApp());
+}
+
+class SmartBookingApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Smart Booking',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  final List<String> categories = [
+    "Restaurants",
+    "Pubs",
+    "Medical Shops",
+    "Wine Shops",
+    "Ration Stores",
+    "Groceries"
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Smart Booking - Delhi NCR"),
+      ),
+      body: ListView.builder(
+        itemCount: categories.length,
+        itemBuilder: (context, index) {
+          return Card(
+            margin: EdgeInsets.all(12),
+            child: ListTile(
+              title: Text(categories[index]),
+              trailing: Icon(Icons.arrow_forward_ios),
+              onTap: () {},
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
